780ae6eb7c83be7d46feefbafeae23b2b1d16447
TurRil <lightscape2001@gmail.com>
2016-11-03 19:24:28 +0200

    Source and explanation.

---
+++ Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class Program
+    {
+        static string newline = Environment.NewLine;
+
+        /// <summary>
+        /// Merge all elements in a and b, sorted in descending order.
+        /// </summary>
+        /// <param name="a">An array of int ascending order.</param>
+        /// <param name="b">An array of int ascending order.</param>
+        /// <returns>Returns a merged int array of input arrays sorted descending.</returns>
+        public static int[] MergeAndReorder(int[] a, int[] b) {
+
+            int[] result = new int[0];
+
+            try
+            {
+                int len_a = a.Length;
+                int len_b = b.Length;
+                int len_r = 0;
+
+                result = new int[len_a + len_b];
+
+                while ((len_a != 0) && (len_b != 0))
+                {
+                    if (a[len_a - 1] >= b[len_b - 1])
+                    {
+                        result[len_r++] = a[len_a - 1];
+                        len_a--;
+                    }
+                    else
+                    {
+                        result[len_r++] = b[len_b - 1];
+                        len_b--;
+                    }
+                }
+
+                while (len_a > 0)
+                {
+                    result[len_r++] = a[(len_a--) - 1];
+                }
+
+                while (len_b > 0)
+                {
+                    result[len_r++] = b[(len_b--) - 1];
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                throw e;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Display the results of testing the <see cref="MergeAndReorder"/> against the standard approach.
+        /// <para>Outputs the source arrays (a, b) then the standard approach result and execution time (ms),
+        /// followed by <see cref="MergeAndReorder"/> function output and execution time.</para>
+        /// <para>The result of the equals shows if <see cref="MergeAndReorder"/> produced the desired result.</para>
+        /// </summary>
+        /// <param name="title">Title for this test to be displayed.</param>
+        /// <param name="a">An array of int ascending order.</param>
+        /// <param name="b">An array of int ascending order.</param>
+        /// <returns>Returns a merged int array of input arrays sorted descending.</returns>
+        public static void Test(String title, int[] a, int[] b)
+        {
+            Console.WriteLine(title + newline);
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            // Built in concatenate and sort
+            watch.Start();
+            var z = new int[a.Length + b.Length];
+            a.CopyTo(z, 0);
+            b.CopyTo(z, a.Length);
+            Array.Sort(z,
+                    new Comparison<int>(
+                        (i2, i1) => i1.CompareTo(i2)
+                    ));
+
+            watch.Stop();
+
+            if (z.Length < 50)
+                Console.WriteLine("\t[{0}]", string.Join(", ", z));
+
+            Console.WriteLine("concat-sort: " + watch.ElapsedMilliseconds +"ms" + newline);
+
+            // Assignment merge and sort
+            watch.Restart();
+
+            int[] result = MergeAndReorder(a, b);
+            watch.Stop();
+
+            if (result.Length < 50)
+                Console.WriteLine("\t[{0}]", string.Join(", ", result));
+
+            Console.WriteLine("assignment: " + watch.ElapsedMilliseconds + "ms" + newline);
+
+            Console.WriteLine("Equals: " + result.SequenceEqual(z) + newline);
+        }
+
+        static void Main(string[] args)
+        {
+            int[] a = new int[5] { 1, 3, 5, 7, 9 };
+            int[] b = new int[5] { 2, 4, 6, 8, 10 };
+
+            // Test witha simple [5] [5] array to prove concept
+            Test("Simple", a, b);
+
+            // Generate some random numbers arrays for us to test
+            int n = 50000; // make it big :)
+            int min = 1;
+            int max = n;
+            Random randNum = new Random();
+
+            int[] c = Enumerable
+                .Repeat(0, n)
+                .Select(i => randNum.Next(min, max))
+                .ToArray();
+
+            int[] d = Enumerable
+                .Repeat(0, n)
+                .Select(i => randNum.Next(min, max))
+                .ToArray();
+
+            Array.Sort(c);
+            Array.Sort(d);
+
+            Test("Long ("+ n +")", c, d);
+
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# Assignment
-Small small algorithm test at home.
+Assignment
+==========
+
+Brief
+----------
+
+`public static int[] MergeAndReorder(int[] a, int[] b) { } `
+
+Arrays a and b are both already sorted in ascending order. Your task is to merge
+all elements in a and b, sorted in descending order. You may not use any native
+sorting functionality of C#, such as SortedList or .OrderBy().
+
+Your variables may only be primitives (and obviously an integer array for the result).
+
+You will be assessed according to the efficiency, correctness and clarity of your
+algorithm.
+
+*Please also explain in a separate paragraph in English how your algorithm works.*
+
+
+Algorithm
+----------------
+
+The two source arrays are already sorted in a ascending order, which means that
+the largest values (the ones that we want to at the front of our resulting array)
+is at the end of the source arrays.
+
+So working from the back we compare the values and decide which value to transfer
+to our resulting array.
+
+<code>
+  A: [1, 3, 5, 7, 9]
+  B: [2, 4, 6, 8, 10]
+</code>
+
+In `JavaScript` we can simply remove(`pop`) the elements from the source arrays
+and then add them to the result(`push`).
+
+In `C#` the arrays are defined and resizing arrays is very costly so using position
+variables we traverse the array in reverse order(`--`) and the result array in
+ascending order(`++`).
+
+Finally at the end the result array is filled up by the remaining items in either
+source arrays.
+
+Resulting array:
+
+<code>
+  [10,9,8,7,6,5,4,3,2,1]
+</code>
+
+Testing
+----------------
+
+The testing method takes a title and two source arrays then compares it by running
+it against the native concatenation and sort of an array and the MergeAndReorder
+function.
